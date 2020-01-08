b86ed647cb93cd9e4826f1162491e34c0b285d51
mookum <mookum@codejunkie.be>
2020-01-08 17:00:53 +0100

    Add todo's

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+/**
+ * Startdate: 08-01-2020
+ * Creator: Porschmann David
+ *
+ * Budget app
+ * ------------------
+ *
+ * This app will keep track of our income and outgoing budget.
+ *
+ * --------------------------------------------
+ * TODO: [] Add event handler
+ * TODO: [] Get input values
+ * TODO: [] Add the new item to our data structure
+ * TODO: [] Add the new item to the UI
+ * TODO: [] Calculate budget
+ * TODO: [] Update the UI
+ * --------------------------------------------
+ *
+ * Modules
+ * ------------------
+ * Data module: data manipulation module
+ * UI module: manipulation module
+ * Controller module: controle the entire app
+ * --------------------------------------------
+ *
+ */
+
